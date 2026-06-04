c09dd8cd40dcc316e64d588c22faa32f495b2903
Mpashi Makgabo <mpashi365@gamil.com>
2026-06-04 17:16:00 +0200

    created validation scriot and got form input elements

---
+++ validation.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("signupForm");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const terms = document.getElementById("terms");
